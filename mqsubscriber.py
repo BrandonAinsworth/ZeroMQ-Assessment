4eb4ef03498b1501e58ac4409727c5549db9981a
Brandon Ainsworth <brandonaainsworth@gmail.com>
2023-09-19 19:41:24 -0600

    Add lat long, except finally functionality

--- mqsubscriber.py
+++ mqsubscriber.py
@@ -9,15 +9,24 @@ import zmq
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to hello world server…")
+print("Connecting to 0MQ publisher…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
+try:
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print(f"Sending request {request} …")
-    socket.send(b"Hello")
+    for request in range(10):
+        print(f"Sending request {request} …")
+        socket.send(b"Latitude and longitude please!")
 
-    #  Get the reply.
-    message = socket.recv()
-    print(f"Received reply {request} [ {message} ]")
+        #  Get the reply.
+        message = socket.recv()
+        print(f"Received reply {request} , {message} ")
+
+        
+#Handle Ctrl+C SIGINT from 0MQ Docs    
+except KeyboardInterrupt:
+    print(' Program Interrupted by User')
+finally:
+    socket.close()
+    context.term()
