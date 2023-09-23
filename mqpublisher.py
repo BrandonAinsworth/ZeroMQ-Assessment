26037e1a341f4a363ec5d760fbafcc05e41de3d3
Brandon Ainsworth <brandonaainsworth@gmail.com>
2023-09-23 09:37:41 -0600

    Functioning pubsub model

--- mqpublisher.py
+++ mqpublisher.py
@@ -1,9 +1,8 @@
+# Pathological publisher
 
-#Author: Guillaume Aubert (gaubert) <guillaume(dot)aubert(at)gmail(dot)com>
-   
-import threading
-import random
+import time
 import zmq
+import random
 
 def generate_random_lat_lon():
     
@@ -15,61 +14,22 @@ def generate_random_lat_lon():
         'long': long
     }
 
-def worker_routine(worker_url: str,
-                   context: zmq.Context = None):
-    """Worker routine"""
-    context = context or zmq.Context.instance()
-
-    # Socket to talk to dispatcher
-    socket = context.socket(zmq.REP)
-    socket.connect(worker_url)
+def main():
+    ctx = zmq.Context.instance()
+    publisher = ctx.socket(zmq.PUB)
+    publisher.bind("tcp://*:5556")
+    # Ensure subscriber connection has time to complete
+    time.sleep(1)
 
     while True:
-        
+        # Send one random update per second
+        try:
+            time.sleep(1)
+            latlong = generate_random_lat_lon()
+            publisher.send_json(latlong)
+        except KeyboardInterrupt:
+            print (" Interrupted by User")
+            break
 
-        string = socket.recv()
-        print(f"Received request: {string} ")
-        # Send reply back to client
-        random_lat_long = generate_random_lat_lon()
-        #  Send reply back to client
-        socket.send_json(random_lat_long)
-                
-            
-
-def main():
-    
-    try:
-    
-        """Server routine"""
-
-        url_worker = "inproc://workers"
-        url_client = "tcp://localhost:5560"
-
-        # Prepare our context and sockets
-        context = zmq.Context.instance()
-
-        # Socket to talk to clients
-        clients = context.socket(zmq.ROUTER)
-        clients.connect(url_client)
-
-        # Socket to talk to workers
-        workers = context.socket(zmq.DEALER)
-        workers.bind(url_worker)
-        
-        # Launch pool of worker threads
-        for i in range(5):
-            thread = threading.Thread(target=worker_routine, args=(url_worker,))
-            thread.daemon = True
-            thread.start()
-
-        zmq.proxy(clients, workers)
-    
-    except KeyboardInterrupt:
-        print(' Program Interrupted by User')
-    finally:
-        clients.close()
-        workers.close()
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
